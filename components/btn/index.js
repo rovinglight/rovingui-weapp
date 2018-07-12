113a2976dab0861eed71b6dac7314ef0022feac0
Billy <646017375@qq.com>
2018-07-12 11:38:40 +0800

    btn prop

--- components/btn/index.js
+++ components/btn/index.js
@@ -1,6 +1,22 @@
 Component({
+    externalClasses: ['ext-class'],
     properties: {
-
+        disabled: {
+            type: Boolean,
+            value: false
+        },
+        size: {
+            type: String,
+            value: 'default'
+        },
+        type: {
+            type: String,
+            value: 'default'
+        },
+        raised : {
+            type: Boolean,
+            value: false
+        }
     },
     data: {
         ripple: [],
@@ -8,12 +24,17 @@ Component({
     },
     methods: {
         handleTap: function (e) {
-            console.log(this.properties)
-            let coordinate = {
-                ...e.detail,
-                key: e.timeStamp
+            console.log(e)
+            if (!this.data.disabled) {
+                let x = e.detail.x - e.target.offsetLeft
+                let y = e.detail.y - e.target.offsetTop
+                let coordinate = {
+                    x: x,
+                    y: y,
+                    key: e.timeStamp
+                }
+                this.generateRipple(coordinate)
             }
-            this.generateRipple(coordinate)
             console.log(JSON.parse(JSON.stringify(this.data.ripple)))
         },
         handleTouchStart: function (e) {
