69f804b94bf4783da86ca7abe13010caa0a08abf
Billy <646017375@qq.com>
2018-07-12 14:14:02 +0800

    sass workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 const gulp = require('gulp')
 const del = require('del')
+const sass = require('gulp-sass')
+const replace = require('gulp-replace')
 
 const path = {
+    src: {
+        scss: 'components/**/*.scss',
+        js: 'components/**/*.js',
+        wxml: 'compponents/**/*.wxml',
+        json: 'components/**/*.json'
+    },
     components_src: [
-        'components/***/*'
+        'components/**/*'
     ],
     components_example: 'example/components'
 }
 
-gulp.task('component_carrier', ['component_cleaner'], () => {
-    gulp.src(path.components_src)
+gulp.task('sass_compiler', ['component_cleaner'], () => {
+    gulp.src(path.src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(replace('.scss', '.wxss'))
+        .pipe(gulp.dest(path.components_example))
+})
+
+gulp.task('component_carrier', ['sass_compiler'], () => {
+    gulp.src([path.src.wxml, path.src.json, path.src.js])
         .pipe(gulp.dest(path.components_example))
 })
 
