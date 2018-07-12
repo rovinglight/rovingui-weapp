81a039768e9ace3719e8ef3e0fda70dcc23d6390
Billy <646017375@qq.com>
2018-07-12 17:16:45 +0800

    button example page

---
+++ example/pages/button/index.js
@@ -0,0 +1,18 @@
+
+Page({
+  data: {
+    
+  },
+
+  onLoad: function () {
+
+  },
+
+  onShow: function() {
+  },
+  handleTap: function (e) {
+    this.setData({
+      loading: true
+    })
+  }
+})
