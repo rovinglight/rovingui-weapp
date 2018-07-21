d8ab657d50d80996492f81426bd41a8382c23de7
Billy <646017375@qq.com>
2018-07-21 14:23:11 +0800

    about page

--- example/pages/main/index.js
+++ example/pages/main/index.js
@@ -1,18 +1,44 @@
+import rvToastFactory from "../../components/toast/toast"
+let rvToast = {}
 
 Page({
   data: {
-    
+    showGithubPage: false
   },
   onLoad: function () {
-
+    rvToast = rvToastFactory(this)
   },
   onShow: function() {
 
+  },
+  onShareAppMessage: function (res) {
+    return {
+      title: 'RovingUI组件库',
+      path: '/pages/main/index'
+    }
   },
   navigateTo: function (e) {
     let pageUrl = e.target.dataset.url
     wx.navigateTo({
       url: pageUrl
+    })
+  },
+  showGithubPage: function () {
+    this.setData({
+      showGithubPage: true
+    })
+  },
+  closeGithubPage: function () {
+    this.setData({
+      showGithubPage: false
+    })
+  },
+  copyUrl: function () {
+    wx.setClipboardData({
+      data: "https://github.com/rovinglight/rovingui-weapp",
+      success: function () {
+        rvToast.show("已将RovingUI的Github地址藏进了你的剪贴板。")
+      }
     })
   }
 })
